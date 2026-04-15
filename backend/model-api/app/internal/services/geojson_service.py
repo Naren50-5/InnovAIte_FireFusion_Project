ea5275d5ad2837e267c551e55750e0e49207de7f
Darren Batarina <batarina.darren@gmail.com>
2026-04-15 10:52:33 +1000

    model api now listens to forecast queue, on message creates mock geojson and sends to prediction queue

--- backend/model-api/app/internal/services/geojson_service.py
+++ backend/model-api/app/internal/services/geojson_service.py
@@ -1,21 +1,17 @@
 import json
 import random
 from pathlib import Path
+from ...models.geojson_model import FeatureCollection
 
 
 class GeoJsonService:
     def __init__(self):
         pass
 
-
-    def get_geojson(self):
-
+    def get_geojson(self) -> FeatureCollection:
         choice = str(random.randint(0, 9))
 
         data_file = Path(__file__).resolve().parents[2] / "data" / f"geojson_data-{choice}.json"
         data = json.loads(data_file.read_text())
 
-        return {
-            "type": "FeatureCollection",
-            "features": data
-        }
+        return FeatureCollection(**data)
